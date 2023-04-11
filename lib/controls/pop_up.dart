1f9f03c818e8948c8f6a77a2be5d7ec61f52f73f
Bambino95 <59890944+kagisomautla@users.noreply.github.com>
2023-04-11 11:23:05 +0200

    Create pop_up.dart

---
+++ lib/controls/pop_up.dart
@@ -0,0 +1,72 @@
+import 'package:credit_card_validator/controls/text.dart';
+import 'package:flutter/material.dart';
+
+Future<dynamic> popupControl({
+  required BuildContext context,
+  required String message,
+  required String title,
+  GestureTapCallback? onConfirm,
+}) async {
+  return await showDialog<void>(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Column(
+          children: [
+            TextControl(
+              text: title,
+              size: TextProps.md,
+              isBold: true,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        contentPadding: EdgeInsets.all(20),
+        actionsPadding: EdgeInsets.all(0),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextControl(
+              text: message,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: onConfirm,
+                  child: Container(
+                    color: Colors.black,
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: TextControl(
+                        text: 'Yes',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Container(
+                    color: Colors.black,
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: TextControl(
+                        text: 'No',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    },
+  );
+}
